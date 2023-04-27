5413d7f1689ed28d4b93e1d2b769e907359cfcbe
harkinder-dark <victorvaddely@gmail.com>
2023-04-27 15:12:11 +0000

    100

---
+++ 0x00-python_variable_annotations/101-main.py
@@ -0,0 +1,6 @@
+safely_get_value = __import__('101-safely_get_value').safely_get_value
+annotations = safely_get_value.__annotations__
+
+print("Here's what the mappings should look like")
+for k, v in annotations.items():
+    print( ("{}: {}".format(k, v)))
--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -8,7 +8,7 @@ from typing import Any, Union, TypeVar, Mapping
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[~T, None] = None) -> Union[Any, ~T]:
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
     """
     dct: Mapping
     """
