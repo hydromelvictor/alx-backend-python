ac25da360d0daac54342a51014592d419ae8d023
harkinder-dark <victorvaddely@gmail.com>
2023-04-27 15:00:56 +0000

    8-make

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -4,7 +4,7 @@ Write a type-annotated function make_multiplier that takes
 a float multiplier as argument and returns a function that
 multiplies a float by multiplier.
 """
-from collections.abc import Callable
+from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
