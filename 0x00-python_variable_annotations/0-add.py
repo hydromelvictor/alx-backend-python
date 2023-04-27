f3205e24255208a31e2b654c98c3133f21f135e9
harkinder-dark <victorvaddely@gmail.com>
2023-04-27 14:13:05 +0000

    0-add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -3,6 +3,8 @@
 Write a type-annotated function add that takes a float a
 and a float b as arguments and returns their sum as a float.
 """
+
+
 def add(a: float, b: float) -> float:
     """
     a : flaot
--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -4,6 +4,7 @@ Write a type-annotated function concat that takes a string str1
 and a string str2 as arguments and returns a concatenated string
 """
 
+
 def concat(str1: str, str2: str) -> str:
     """
     str1 : str
