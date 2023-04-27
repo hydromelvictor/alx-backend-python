8bbfdee0c0bf7d2e6104a1cfc9fe57bde69bff0d
harkinder-dark <victorvaddely@gmail.com>
2023-04-27 15:48:31 +0000

    add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -5,10 +5,12 @@ takes a float a and a float b as
 arguments and returns their sum as a
 float.
 """
+
+
 def add(a: float, b: float) -> float:
     """
     a: float
     b: float
     return : float
     """
-    return float(a + b)
+    return a + b
