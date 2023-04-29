165d99b2585b9a477e2bd1a36493905bf0839cfc
harkinder-dark <victorvaddely@gmail.com>
2023-04-29 09:48:53 +0000

    task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,18 +2,30 @@
 """
 You will spawn wait_random n times with the specified max_delay.
 """
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def sorting(lst: list) -> list:
+def little(lst: List[float]) -> float:
+    """
+    lst : List[float]
+    """
+    min = lst[0]
+    for i in lst:
+        if min > i:
+            min = i
+    return min
+
+
+def sorting(lst: List[float]) -> list:
     """
     lst : list
     """
     r: list = lst[:]
     s = []
     for i in range(len(lst)):
-        s.append(min(r))
-        r.pop(min(r))
+        s.append(little(r))
+        r.remove(little(r))
     return s
 
 
