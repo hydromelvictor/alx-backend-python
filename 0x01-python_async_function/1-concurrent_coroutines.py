8e49c2367b8d9be30941bed5059dabc65f429da6
harkinder-dark <victorvaddely@gmail.com>
2023-04-29 09:17:43 +0000

    modify task -1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,6 +5,18 @@ You will spawn wait_random n times with the specified max_delay.
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
+def sorting(lst: list) -> list:
+    """
+    lst : list
+    """
+    r: list = lst[:]
+    s = []
+    for i in range(len(lst)):
+        s.append(min(r))
+        r.pop(min(r))
+    return s
+
+
 async def wait_n(n: int, max_delay: int) -> list:
     """
     n : int
@@ -14,4 +26,4 @@ async def wait_n(n: int, max_delay: int) -> list:
     r = []
     for i in range(n):
         r.append(await wait_random(max_delay))
-    return sorted(r)
+    return sorting(r)
