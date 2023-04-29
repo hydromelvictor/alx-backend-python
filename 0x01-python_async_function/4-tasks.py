43b5907f7cf51607870144d1585197ecffb69740
harkinder-dark <victorvaddely@gmail.com>
2023-04-29 10:05:07 +0000

    task 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -3,10 +3,12 @@
 Take the code from wait_n and alter it into
 a new function task_wait_n.
 """
+from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
+sorting = __import__('1-concurrent_coroutines').sorting
 
 
-async def task_wait_n(n: int, max_delay: int) -> list:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     n : int
     max_delay : int
@@ -15,4 +17,4 @@ async def task_wait_n(n: int, max_delay: int) -> list:
     r = []
     for i in range(n):
         r.append(await task_wait_random(max_delay))
-    return sorted(r)
+    return sorting(r)
