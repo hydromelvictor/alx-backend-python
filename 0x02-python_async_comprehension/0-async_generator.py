ee7b0ec85ef2f3e9b2c45bb3d85e92af161a3c82
harkinder-dark <victorvaddely@gmail.com>
2023-05-02 13:33:57 +0000

    tasck 1

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -12,5 +12,5 @@ async def async_generator() -> float:
     _return_ : 
     """
     for i in range(10):
-        await asyncio.sleep(1)
         yield uniform(0, 10)
+        await asyncio.sleep(1)
